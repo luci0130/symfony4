de63272b1f72dba83a4b7fc90a777d1822749c38
Lucian Turiac <turiac.lucian@isoftbet.com>
2019-02-06 09:35:45 +0200

    Routes, Controllers, Twig, Symfony Flex

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: isbb 110
+ * Date: 2/4/2019
+ * Time: 5:26 PM
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('Im 4skllsdl');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug)
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments
+        ]);
+    }
+}
