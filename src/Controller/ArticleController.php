d31477a0af722c4f5a35752033d05e2840ae4502
Lucian Turiac <turiac.lucian@isoftbet.com>
2019-02-06 15:31:35 +0200

    Stellar Development with Symfony 4

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,22 +8,23 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('Im 4skllsdl');
+        return $this->render("article/homepage.html.twig");
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
@@ -35,7 +36,18 @@ class ArticleController extends AbstractController
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
             'comments' => $comments
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart(LoggerInterface $logger)
+    {
+        // TODO - acutally heart/unheart the article!
+        $logger->info('Execute toggleArticleHeart');
+        return new JsonResponse(['hearts' => rand(5,100)]);
     }
 }
