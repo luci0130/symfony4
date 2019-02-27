5b59947976563c78d3cc7339f4a45e4dd2e7900d
Lucian Turiac <turiac.lucian@isoftbet.com>
2019-02-27 12:10:28 +0200

    Mastering Doctrine Relations! - Fetching Relations

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,6 +10,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use App\Repository\CommentRepository;
 use App\Service\MarkdownHelper;
 use App\Service\SlackClient;
 use Doctrine\ORM\EntityManagerInterface;
@@ -50,23 +51,16 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show(Article $article, SlackClient $slackClient, EntityManagerInterface $em)
+    public function show(Article $article, SlackClient $slackClient)
     {
         if ($article->getSlug() == "Luci") {
             $slackClient->sendMessage('John Doe', 'This is an amazing message!');
         }
 
-        $comments = [
-            'I ate a normal rock once. It did NOT taste like bacon!',
-            'Woohoo! I\'m going on an all-asteroid diet!',
-            'I like bacon too! Buy some from my site! bakinsomebacon.com',
-        ];
-
         return $this->render(
             'article/show.html.twig',
             [
                 'article' => $article,
-                'comments' => $comments,
             ]
         );
     }
